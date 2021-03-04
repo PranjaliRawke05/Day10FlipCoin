6f4c54314d379501e020d8fe857bc33a30d8a43f
PranjaliRawke05 <Pranjaliprawke@gmail.com>
2021-03-03 18:01:40 -0800

    Number of times head & tails won

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-coin=$((RANDOM%2))
-echo $coin
+c=0;c1=0;
+for (( i=0; i<10; i++))
+do
+	coin=$((RANDOM%2))
+	echo $coin
+	if [ $coin -eq 1 ]
+	then
+		c=$(($c+1))
+	else
+		c1=$(($c1+1))
+	fi
+done
+echo "Number of times head win"$c
+echo "Number of times tails win"$c1
