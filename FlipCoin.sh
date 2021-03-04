9d2cc06cc712dbcfb1226cde0488507c65942591
PranjaliRawke05 <Pranjaliprawke@gmail.com>
2021-03-03 18:05:26 -0800

    Tie or Win

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 c=0;c1=0;
-for (( i=0; i<10; i++))
+while [ $c -le 21 -o $c1 -le 21 ]
 do
 	coin=$((RANDOM%2))
 	echo $coin
@@ -12,5 +12,14 @@ do
 		c1=$(($c1+1))
 	fi
 done
-echo "Number of times head win"$c
-echo "Number of times tails win"$c1
+echo "Number of times head won"$c
+echo "Number of times tails won"$c1
+if [ $c -eq $c1 ]
+then
+	echo "Its a Tie"
+elif [ $c -gt $c1 ]
+then
+	echo "Head won"
+else
+	echo "Tails won"
+fi
